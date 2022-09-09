f8b18ff72dd29fc7051c511ecf126c2db1c21c62
root <williams.victor15@yahoo.com>
2022-09-09 16:45:30 -0700

    Print F

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main- printthe string in the printf function
+ * main - print the string in the printf function
  *
  * Description: using the main function
  * the program prints with proper grammar, but the outcome is a piece of art,
- * Return:0
+ * Return: 0
  */
 int main(void)
 {
-	print f("with proper grammar, but the outcome is a piece of art,\n");
+	printf ("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
