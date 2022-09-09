c08fca7dd2a76eb2a4593a1d9ea4d1389ae69e04
root <williams.victor15@yahoo.com>
2022-09-09 16:16:17 -0700

    putschar

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/* 
- *this program prints "Programming is like building a multilingual puzzle
- *Return:0
+/** 
+ * this program prints "Programming is like building a multilingual puzzle
+ * Return:0
+ *
+ * Description: The func puts prints the string above
  */
 int main(void)
 {
