360f475430f2b92e453a7997cdecefd615ef9ef9
root <williams.victor15@yahoo.com>
2022-09-09 16:06:02 -0700

    puts char

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
  *this program prints "Programming is like building a multilingual puzzle
  *Return:0
  */
-int main ()
+int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts ("\'"Programming is like building a multilingual puzzle")
+	return (0);
 }
