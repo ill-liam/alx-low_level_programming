80d793b121be9426d9d43ff3165c1874125627cd
root <williams.victor15@yahoo.com>
2022-09-12 07:34:40 -0700

    if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,11 +19,11 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is zero\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("%d is  negative\n", n);
+		printf("%d is negative\n", n);
 	}
 	return (0);
 }
