11f196eba6dd5313d26f033f9fdb5bf9b72f2537
root <williams.victor15@yahoo.com>
2022-09-13 03:00:26 -0700

    Combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combination
+ *
+ * Description: usih the main function
+ * this program prints all the possible combination
+ * of a single digit number
+ * followed by a new line, comma and space
+ * Return: 0
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	{
+		putchar(',');
+	}
+	{
+		putchar(' ');
+	}
+	{
+		putchar('\n');
+	}
+	return (0);
+}
