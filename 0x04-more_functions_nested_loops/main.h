15dfcfa6336af563f2c51ec1ee8dfa72faa04a34
root <williams.victor15@yahoo.com>
2022-09-16 14:38:06 -0700

    task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,12 +1,15 @@
-i#ifndef HEADER_FILE
-#define HEADER_FILE
+i#ifndef MAIN_H
+#define MAIN_H
+/**
+ * main - header file for protoypes
+ * void protoypes (void)
+ * int protoypes (int)
+ */
 
-#include <stdio.h>
-
-int _isupper(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int nmul(int a, int b);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -15,4 +18,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_line(int n);
-#endif
+void print_number(int n);
+#endif /* MAIN_H */
