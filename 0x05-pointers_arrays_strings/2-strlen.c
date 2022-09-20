b1d120c278bfeaff2edb40520261b8f318862a25
root <williams.victor15@yahoo.com>
2022-09-20 13:54:49 -0700

    task3

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: returns length as integer;
+ */
+Return: returns length as integer;
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
